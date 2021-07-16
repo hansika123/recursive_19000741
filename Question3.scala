9a56da23e6a2671f5197b4aa5dfa805085faefcc
hansika123 <62392179+hansika123@users.noreply.github.com>
2021-07-17 02:04:36 +0530

    Add files via upload

---
+++ Question3.scala
@@ -0,0 +1,13 @@
+//Question 3
+
+object Question3 extends App {
+    def sum(num:Int):Int={
+      num match {
+        case 1  => num
+        case _  => num+sum(num-1)
+      }
+    }
+    println("\n*** Addition of numbers from1 to n ***")
+    println(sum(5))
+
+}
